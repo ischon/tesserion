cf2dea02c46890d4e3ec8f3eb83ce8f78b63dc3c
Ian Schön <ian.schon@notive.nl>
2025-12-21 17:14:01 +0100

    Revert "chore: create functions for login"
    This reverts commit f012a581e06e43ebd961b66c498d731e25c0477a.

--- functions/__/firebase/[...path].js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-    const url = new URL(context.request.url);
-    const firebaseHost = "schon-tesserion.firebaseapp.com";
-
-    const proxyUrl = "https://" + firebaseHost + url.pathname + url.search;
-
-    return fetch(new Request(proxyUrl, context.request));
-}
--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: "tesserion.schon.dev",
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
